0a5a545519aa6a04347e30491d214405fb8ef0c6
Andreia Oliveira <andreiaoliveira@Andreias-MacBook-Pro.local>
2017-05-03 20:47:53 +0100

    Initial Commit

---
+++ Xcode 8 second course/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode 8 second course
+//
+//  Created by Andreia Oliveira on 03/05/2017.
+//  Copyright © 2017 Andreia Oliveira. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Xcode 8 second course/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Xcode 8 second course
+//
+//  Created by Andreia Oliveira on 03/05/2017.
+//  Copyright © 2017 Andreia Oliveira. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
